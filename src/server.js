36e0991ed7b1a3209ed0b65888f5f18c2aabb2bf
Matias Dumrauf <matias.dumrauf@gmail.com>
2023-03-08 19:29:31 -0300

    feat: parameterize server PORT with env var

--- src/server.js
+++ src/server.js
@@ -1,12 +1,14 @@
 /* eslint-disable no-console */
 const app = require('./app');
 
+const PORT = parseInt(process.env.PORT || '3001', 10);
+
 init();
 
 async function init() {
   try {
-    app.listen(3001, () => {
-      console.log('Express App Listening on Port 3001');
+    app.listen(PORT, () => {
+      console.log(`Express App Listening on Port ${PORT}`);
     });
   } catch (error) {
     console.error(`An error occurred: ${JSON.stringify(error)}`);
