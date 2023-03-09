c86cad8814ce5751341c8c95d13b213a4d398151
Matias Dumrauf <matias.dumrauf@gmail.com>
2023-03-09 16:43:37 -0300

    fix: test any date in contracts test

--- src/routes/contracts.test.js
+++ src/routes/contracts.test.js
@@ -3,26 +3,23 @@ const app = require('../app');
 
 describe('Contracts API', () => {
   describe('GET /contracts/:id', () => {
-    it('returns a contract', (done) => {
-      request(app)
+    it('returns a contract', async () => {
+      const response = await request(app)
         .get('/contracts/8')
         .set('Accept', 'application/json')
-        .set('profile_id', '4')
-        .expect(
-          200,
-          {
-            id: 8,
-            createdAt: expect.any(String),
-            updatedAt: expect.any(String),
-            status: 'in_progress',
-            terms: 'bla bla bla',
-            clientId: 4,
-            client: 'Ash Kethcum',
-            contractorId: 6,
-            contractor: 'Linus Torvalds',
-          },
-          done
-        );
+        .set('profile_id', '4');
+      expect(response.status).toBe(200);
+      expect(response.body).toMatchObject({
+        id: 8,
+        createdAt: expect.any(String),
+        updatedAt: expect.any(String),
+        status: 'in_progress',
+        terms: 'bla bla bla',
+        clientId: 4,
+        client: 'Ash Kethcum',
+        contractorId: 6,
+        contractor: 'Linus Torvalds',
+      });
     });
 
     it('fails with 404 if contract does not exist', (done) => {
@@ -55,74 +52,68 @@ describe('Contracts API', () => {
   });
 
   describe('GET /contracts', () => {
-    it('returns all non-terminated contracts for client', (done) => {
-      request(app)
+    it('returns all non-terminated contracts for client', async () => {
+      const response = await request(app)
         .get('/contracts')
         .set('Accept', 'application/json')
-        .set('profile_id', '1')
-        .expect(
-          200,
-          [
-            {
-              id: 2,
-              createdAt: expect.any(String),
-              updatedAt: expect.any(String),
-              status: 'in_progress',
-              terms: 'bla bla bla',
-              clientId: 1,
-              client: 'Harry Potter',
-              contractorId: 6,
-              contractor: 'Linus Torvalds',
-            },
-          ],
-          done
-        );
+        .set('profile_id', '1');
+      expect(response.status).toBe(200);
+      expect(response.body).toMatchObject([
+        {
+          id: 2,
+          createdAt: expect.any(String),
+          updatedAt: expect.any(String),
+          status: 'in_progress',
+          terms: 'bla bla bla',
+          clientId: 1,
+          client: 'Harry Potter',
+          contractorId: 6,
+          contractor: 'Linus Torvalds',
+        },
+      ]);
     });
 
-    it('returns all non-terminated contracts for contractor', (done) => {
-      request(app)
+    it('returns all non-terminated contracts for contractor', async () => {
+      const response = await request(app)
         .get('/contracts')
         .set('Accept', 'application/json')
-        .set('profile_id', '6')
-        .expect(
-          200,
-          [
-            {
-              id: 2,
-              createdAt: expect.any(String),
-              updatedAt: expect.any(String),
-              status: 'in_progress',
-              terms: 'bla bla bla',
-              clientId: 1,
-              client: 'Harry Potter',
-              contractorId: 6,
-              contractor: 'Linus Torvalds',
-            },
-            {
-              id: 3,
-              createdAt: expect.any(String),
-              updatedAt: expect.any(String),
-              status: 'in_progress',
-              terms: 'bla bla bla',
-              clientId: 2,
-              client: 'Mr Robot',
-              contractorId: 6,
-              contractor: 'Linus Torvalds',
-            },
-            {
-              id: 8,
-              createdAt: expect.any(String),
-              updatedAt: expect.any(String),
-              status: 'in_progress',
-              terms: 'bla bla bla',
-              clientId: 4,
-              client: 'Ash Kethcum',
-              contractorId: 6,
-              contractor: 'Linus Torvalds',
-            },
-          ],
-          done
-        );
+        .set('profile_id', '6');
+      expect(response.status).toBe(200);
+      expect(response.body).toMatchObject([
+        {
+          id: 2,
+          createdAt: expect.any(String),
+          updatedAt: expect.any(String),
+          status: 'in_progress',
+          terms: 'bla bla bla',
+          clientId: 1,
+          client: 'Harry Potter',
+          contractorId: 6,
+          contractor: 'Linus Torvalds',
+        },
+        {
+          id: 3,
+          createdAt: expect.any(String),
+          updatedAt: expect.any(String),
+          status: 'in_progress',
+          terms: 'bla bla bla',
+          clientId: 2,
+          client: 'Mr Robot',
+          contractorId: 6,
+          contractor: 'Linus Torvalds',
+        },
+        {
+          id: 8,
+          createdAt: expect.any(String),
+          updatedAt: expect.any(String),
+          status: 'in_progress',
+          terms: 'bla bla bla',
+          clientId: 4,
+          client: 'Ash Kethcum',
+          contractorId: 6,
+          contractor: 'Linus Torvalds',
+        },
+      ]);
     });
 
     it('returns empty array when contractor has no non-terminated contracts', (done) => {
